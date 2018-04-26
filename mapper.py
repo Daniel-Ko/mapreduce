67211a50d958f5d214d0807b06ccd30ba88c73e5
Dan.Ko <daniel.j.ko@gmail.com>
2018-04-26 18:02:16 +1200

    Fixed mapper.py so it now does wordcount, and read a csv file properly. Please pass in csv

--- mapper.py
+++ mapper.py
@@ -3,41 +3,43 @@ from concurrent import futures
 from itertools import repeat
 import argparse
 from os import path
+import csv
 
-
-def get_key_val_from_str(key_ind, val_ind, line):
+def get_key_val_from_str(key_ind, tokens):
     """ Use to get the tokens you need from a line of data """
-    tokens = line.split()
-    return (tokens[key_ind], tokens[val_ind])
+    return tokens[key_ind]
 
-def parse_and_map(fname, key_ind, val_ind, num_processes, maps_per_node):
+def parse_and_map(fname, key_ind, num_processes, maps_per_node):
     """Processes the data file concurrently
 
         Args: 
             fname (String): filename
             key_ind (int): use to determine the column in the data to be the key
-            val_ind (int): use to determine the column in the data to be mapped as value
             num_processes: the amount of processes that should run concurrently to map the data
             maps_per_node: number of maps total each process should do
 
         Returns:
-            data (list): tuples containing key data entry and value data entry
+            data (dict): tuples containing key data entry mapped to the frequency of it appearing in the datafile
     """
     if num_processes <= 0:
         num_processes = None # If file size is too small, just use max processors on machine
 
-    data = []
+    data = {}
 
     # Create repeating iterators for continuous parameters to the map function
     key_repeater = repeat(key_ind)
-    val_repeater = repeat(val_ind)
 
-    with open(fname, "r") as f:   
+    with open(fname, "r") as csvf:
+        reader = csv.reader(csvf)   
         try:
             with futures.ProcessPoolExecutor(int(num_processes)) as executor:
-                mapper_gen = executor.map(get_key_val_from_str, key_repeater, val_repeater, f, timeout=1, chunksize=maps_per_node)
-                for pair in mapper_gen:
-                    data.append(pair)
+                mapper_gen = executor.map(get_key_val_from_str, key_repeater, reader, timeout=1, chunksize=maps_per_node)
+                
+                for key in mapper_gen:
+                    if key in data:
+                        data[key] += 1
+                    else:
+                        data[key] = 0
         except TimeoutError:
             print("Took too long")
             pass
@@ -53,13 +55,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='File and data indices to map')
     parser.add_argument('fname', help="Need a filename to read from")
     parser.add_argument('key_ind', type=int, help="column num from data you want to be the key")
-    parser.add_argument('val_ind', type=int, help="column num from data you want to be mapped as the value")
+    # parser.add_argument('val_ind', type=int, help="column num from data you want to be mapped as the value")
     parser.add_argument('size_of_node', type=float, help="Block size of a single node in MB")
     parser.add_argument('maps_per_node', nargs='?', type=int, help="Maps generated per node?", default=10)
     args = parser.parse_args()
 
     num_processes = calculate_num_processes_needed(args.fname, args.size_of_node, args.maps_per_node)
-    data = parse_and_map(args.fname, args.key_ind, args.val_ind, num_processes, args.maps_per_node)
+    data = parse_and_map(args.fname, args.key_ind, num_processes, args.maps_per_node)
     
-    for pair in data:
-        print(pair)
+    for key, count in data.items():
+        print("{}, {}".format(key, count))
